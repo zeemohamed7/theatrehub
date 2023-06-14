a0679403a81db71e9e03ed6ebb52b14b1aa1bd93
zeemohamed7 <zainabshmohamed7@gmail.com>
2023-06-14 21:32:36 +0300

    ignore

--- controllers/booking.js
+++ controllers/booking.js
@@ -2,13 +2,6 @@ const Movie = require('../models/Movie')
 const Booking = require('../models/Booking')
 const { render } = require('ejs')
 
-// exports.booking_get = async (req, res) => {
-//     const movie = await Movie.find()
-//     const user = req.user.emailAddress
-//     console.log(movie)
-//     console.log(user)
-//     res.render('movies/detail', {movie, user})
-// }
 
 exports.booking_confirm_post = async(req, res) => {
 try {
